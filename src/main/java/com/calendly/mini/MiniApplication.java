aba8331e5267e1f3cf54fad778a7b7e27a6e4b4e
neeraj.gupta <neeraj.gupta@quikr.com>
2020-04-11 22:31:35 +0530

    first commit with HeroKu & Spring Initialzer

---
+++ src/main/java/com/calendly/mini/MiniApplication.java
@@ -0,0 +1,23 @@
+package com.calendly.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class MiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+}
---
+++ src/test/java/com/calendly/mini/MiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendly.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
