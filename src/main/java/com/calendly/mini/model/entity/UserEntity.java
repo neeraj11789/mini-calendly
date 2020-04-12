dcc9b97028be5707b2c1068985f1d82c8322e5db
neeraj.gupta <neeraj.gupta@quikr.com>
2020-04-13 01:11:52 +0530

    Optimized Imports

--- src/main/java/com/calendly/mini/model/entity/UserEntity.java
+++ src/main/java/com/calendly/mini/model/entity/UserEntity.java
@@ -2,7 +2,10 @@ package com.calendly.mini.model.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Table(name = "users")
 @Entity
